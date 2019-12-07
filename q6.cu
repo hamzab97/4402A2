3ba4b469d72be184937b740df17048a4d64a9529
hamzab97 <hbana3@uwo.ca>
2019-12-07 14:15:03 -0500

    shared mem fixed with blockdim

--- q6.cu
+++ q6.cu
@@ -14,7 +14,7 @@ __global__ void minplus(int n, int* x, int *y)
 	//declare shared memory DS
 	extern __shared__ int tile_x[blockDim][blockDim];
 
-	int (m = 0; m < n/blockDim+1; ++m){
+	int (int m = 0; m < n/blockDim+1; ++m){
 		tile_x[threadIdx.x][threadIdx.y] = x[i*n + j];
 
 		int sum = INT_MAX;
