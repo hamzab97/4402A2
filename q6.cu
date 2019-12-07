6388ececdebab75ca0318fa0b30233aafc43de89
hamzab97 <hbana3@uwo.ca>
2019-12-07 17:37:04 -0500

    fixed errors shared mem

--- q6.cu
+++ q6.cu
@@ -15,7 +15,7 @@ __global__ void minplus(int n, int* x, int *y)
 	//declare shared memory DS
 	__shared__ int tile_x[BLOCK_SIZE][BLOCK_SIZE];
 
-	int (int m = 0; m < n/(BLOCK_SIZE+1); ++m){
+	for (int m = 0; m < n/(BLOCK_SIZE+1); ++m){
 		tile_x[threadIdx.x][threadIdx.y] = x[i*n + j];
 
 		int sum = INT_MAX;
